f41f25dc81358a8f21fb47f4c50609f7769afb63
Derik Vo <DerikVoSanJose@Gmail.com>
2023-06-02 14:27:17 -0700

    removed the class for images for a stand alone function, add default parameters for functions

--- modules/eda.py
+++ modules/eda.py
@@ -2,29 +2,44 @@ from keras.preprocessing.image import ImageDataGenerator
 from math import ceil
 import os
 import random
+import numpy as np
 from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-import numpy as np
-np.random.seed(42)
-from tensorflow.keras.utils import set_random_seed
-set_random_seed(42)
 
-def folders(dataset):
+'''
+The concept of this code was learned from General Assembly's Data science Immersive's Excel lab excercise
+The concepts has been adapted to identify file paths ways to work with the data
+''' 
+def folders(dataset = 'Training'):
+    '''
+    Finds the folders within either the Training dataset or the Testing data set.
+    =============================================================================    
+    Keyword arguments:
+    dataset -- The main folder either Train and Test folder (default Training)
+    '''
     path = f'../Images/{dataset}'
     images_dir = [x for x in os.listdir(path)]
     folders= os.listdir(path)
     return folders
 
-def image_len(dataset, folders):
+
+'''
+
+The concept of this code was learned from General Assembly's Data science Immersive's Excel lab excercise
+The concepts has been adapted to identify file paths ways to work with the data
+'''
+def image_len(folders, dataset ='Training'):
     '''
-    This code takes in the list directory of the folder containing the classification folders. And the dataset.
-    this code was heavily inspired by this project: https://github.com/DerikVo/DSI_project_4_plant_disease/blob/main/notebooks/01_Potato_PlantVillageEDA.ipynb
-    Has since been adapted to work with a jupyter notebook
-    TODO:convert all image eda into a class/method script
+    
+    List subfolders with the main folder containing the classification folders for each image set.
+    =============================================================================    
+    Keyword arguments:
+    folders -- The sub folder containing the classifcation for tumor type
+    dataset -- The main folder either Train and Test folder (default Training)
     '''
     #loop to each sub folder so we can get the class sizes
     path = f'../Images/{dataset}'
@@ -44,41 +59,43 @@ def image_len(dataset, folders):
         plt.imshow(image)
         plt.axis('off')
         plt.show()
-class Image:
-    def __init__(self, dataset, sub_folder):
-        #learned i didnt need a comma because that creates a tuple: https://stackoverflow.com/questions/39192261/class-init-takes-parameters-but-turns-them-into-tuples-for-some-reason
-        self.dataset = dataset
-        self.sub_folder = sub_folder
-    
-    def avg_images(self):
-      '''
-      This function takes two arguments the dataset: training or testing, and the sub_folder for the type of tumor e.g. ['glioma', 'meningioma', 'notumor', 'pituitary']
-      This function is used to find the average pixel values of each class
-      The purpose is to find if there is a difference in each class
-      '''
-      #assign the path in the function for readability and understanding
-      #assign the sub folder (class name) that was passed to the function
-      path = (f'../Images/{self.dataset}')
-      class_name = self.sub_folder
-      batch_size = 32  # Modify this to suit your needs
-      #instantiate ImageDataGenerator
-      datagen = ImageDataGenerator(rescale=1./255)  # normalize pixel values to [0,1]
-      #get the images from the directory
-      generator = datagen.flow_from_directory(path,
-                                              classes=[class_name],
-                                              class_mode=None,
-                                              color_mode='grayscale',
-                                              target_size=(256, 256),
-                                              batch_size=batch_size)
-      n_samples = generator.samples
-      average_image = np.zeros((256, 256, 1))
+'''
+This function takes two arguments the dataset: training or testing, and the sub_folder for the type of tumor e.g. ['glioma', 'meningioma', 'notumor', 'pituitary']
+This function is used to find the average pixel values of each class
+The purpose is to find if there is a difference in each class
+'''
+def avg_images(folders, dataset = 'Training'):
+  '''
 
-      for i in range(n_samples // batch_size):  # Integer division to avoid partial batches
-          images = next(generator)
-          average_image += np.sum(images, axis=0)
+  This function is used to find the average pixel value of each class
+  =============================================================================    
+  Keyword arguments:
+  folders -- The sub folder containing the classifcation for tumor type
+  dataset -- The main folder either Train and Test folder (default Training)
+  '''
+  #assign the path in the function for readability and understanding
+  #assign the sub folder (class name) that was passed to the function
+  path = (f'../Images/{dataset}')
+  class_name = folders
+  batch_size = 32  # Modify this to suit your needs
+  #instantiate ImageDataGenerator
+  datagen = ImageDataGenerator(rescale=1./255)  # normalize pixel values to [0,1]
+  #get the images from the directory
+  generator = datagen.flow_from_directory(path,
+                                          classes=[class_name],
+                                          class_mode=None,
+                                          color_mode='grayscale',
+                                          target_size=(256, 256),
+                                          batch_size=batch_size)
+  n_samples = generator.samples
+  average_image = np.zeros((256, 256, 1))
 
-      average_image /= n_samples
-      return average_image
+  for i in range(n_samples // batch_size):  # Integer division to avoid partial batches
+      images = next(generator)
+      average_image += np.sum(images, axis=0)
+
+  average_image /= n_samples
+  return average_image
     
 def image_contrast(comparision, base_image):
     # we need to rescale the contrasts
@@ -87,3 +104,22 @@ def image_contrast(comparision, base_image):
     image /= image.max() # divide by new max
 
     return image
+
+def display_avg(avg_images, title):
+#plotting the contrast between GLioma and no Tumor
+    fig, ax = plt.subplots(1,3, figsize = (10,10))
+    plt.suptitle(f'Pixel average: {title}', y = .75, fontsize = 22)
+    ax[0].imshow(avg_images)
+    ax[0].set_title('Viridis', fontsize=18)
+    ax[0].axis('off')
+
+    ax[1].imshow(avg_images, cmap='seismic')
+    ax[1].set_title('Seismic', fontsize=18)
+    ax[1].axis('off')
+
+    ax[2].imshow(avg_images, cmap='BrBG')
+    ax[2].set_title('BrBG', fontsize=18)
+    ax[2].axis('off')
+    plt.tight_layout()
+    plt.savefig(f'../Created_images/Pixel_average_{title}.png')
+    plt.show()
